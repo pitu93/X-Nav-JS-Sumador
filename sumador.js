d1c67a705dfe12508a5c23283dd4d48eb7881367
Alejandro Lorenzo Caro <pitu_fuenla93@hotmail.es>
2015-01-30 17:43:06 +0100

    Ejercicio acabado

--- sumador.js
+++ sumador.js
@@ -1,4 +1,14 @@
 function sumador() {
-    var element = document.getElementById("res");
-    element.innerHTML = "= 8";
+    var sumandos = document.getElementById("op").innerHTML;
+    var a = sumandos.split("+");
+    var resultado = document.getElementById("res");
+    resultado.innerHTML = "= " + (parseInt(a[0]) + parseInt(a[1]));
+    
+}
+
+function suma(){
+        var sumandos = document.getElementById("op");
+        sumandos.innerHTML = Math.floor(Math.random()*10)+ " + " + Math.floor(Math.random()*10);
+        var resultado = document.getElementById("res");
+        resultado.innerHTML = "";
 }
